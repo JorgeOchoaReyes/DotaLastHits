07ad152745465f3b6d6af1f8c7ad534546e866fe
Jorge Ochoa Reyes <jorge@bytekitchen.io>
2022-06-18 20:24:08 -0700

    Example map with react native maps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Example Map: </Text>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width/2,
+    height: Dimensions.get('window').height/2,
   },
 });
