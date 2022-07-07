59912d0b8024054b7f57ed9f2b88ca4e34a09c84
Jorge Ochoa Reyes <jorgeochoareyes@gmail.com>
2022-07-06 23:16:06 -0700

    Added drag and drop movement and one creep moving forward

---
+++ components/melee_creep.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface melee_creepProps {
+
+}
+
+export const melee_creep: React.FC<melee_creepProps> = ({}) => {
+    return (
+        <p> Melee </p>
+    );
+}
---
+++ components/ranged_creep.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface ranged_creepProps {
+
+}
+
+export const ranged_creep: React.FC<ranged_creepProps> = ({}) => {
+    return (<p>
+        Ranged Creep
+    </p>);
+}
